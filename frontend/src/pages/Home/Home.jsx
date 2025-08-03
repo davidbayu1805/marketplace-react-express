8b2d74dc4a9980c7ae4efc7fecbccb83bf19d648
davidbayu1805 <davidbayu57@gmail.com>
2025-08-03 16:25:55 +0700

    finish fe

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <div>
         <Header/>
-        <ExploreMenu caegory={category} setCategory={setCategory}/>
+        <ExploreMenu category={category} setCategory={setCategory}/>
         <FoodDisplay category={category}/>
         <AppDownload/>
     </div>
